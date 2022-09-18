e51b19c905dafad00798b2b46fb90a416c21805b
ImRyzon <imtheryzon@gmail.com>
2022-09-18 11:11:40 -0400

    Create Junior 1.java

---
+++ CCC/2010/Junior/Junior 1.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        switch(num) {
+            case 1:
+                System.out.println(1);
+                break;
+                
+            case 2:
+                System.out.println(2);
+                break;
+            case 3: 
+                System.out.println(2);
+                break;
+            case 4:
+                System.out.println(3);
+                break;
+            case 5:
+                System.out.println(3);
+                break;
+            case 6:
+                System.out.println(3);
+                break;
+            case 7:
+                System.out.println(2);
+                break;
+            case 8:   
+                System.out.println(2);
+                break;
+            case 9:
+                System.out.println(1);
+                break;
+            case 10:
+                System.out.println(1);
+                break;
+        }
+    }
+}
