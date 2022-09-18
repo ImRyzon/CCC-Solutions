a8120e21c93d4e031323ed47adb64740c3d4ad9b
ImRyzon <imtheryzon@gmail.com>
2022-09-18 11:08:19 -0400

    Create Senior 1.java

---
+++ CCC/2008/Senior/Senior 1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] line = sc.nextLine().split(" ");
+        String city = line[0];
+        int temp = Integer.parseInt(line[1]);
+        while (!(line[0].equalsIgnoreCase("Waterloo"))) {
+            line = sc.nextLine().split(" ");
+            if (Integer.parseInt(line[1]) < temp) {
+                city = line[0];
+                temp = Integer.parseInt(line[1]);
+            }
+            if (line[0].equalsIgnoreCase("Waterloo")) {
+                break;
+            }
+        }
+        System.out.println(city);
+        sc.close();
+    }
+}
