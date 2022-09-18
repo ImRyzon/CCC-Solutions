19ea4cd7aa20d33546fbf064890ee20de5af3ac3
ImRyzon <imtheryzon@gmail.com>
2022-09-18 11:43:11 -0400

    Create Junior 1.java

---
+++ CCC/2021/Junior/Junior 1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+public class Junior1 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        int B = readInt();
+        int P = (5*B) - 400;
+        System.out.println(P);
+        if (P < 100) {
+            System.out.println(1);
+        } else if (P == 100) {
+            System.out.println(0);
+        } else {
+            System.out.println(-1);
+        }
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
