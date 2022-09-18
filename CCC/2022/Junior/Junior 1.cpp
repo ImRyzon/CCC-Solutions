ad3f154ff0c67d63e48fd2608b038529206cb694
ImRyzon <imtheryzon@gmail.com>
2022-09-18 11:47:19 -0400

    Create Junior 1.cpp

---
+++ CCC/2022/Junior/Junior 1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize ("Ofast")
+#pragma GCC target ("avx2")
+#define pb push_back
+#define pf push_front
+#define ef emplace_front
+#define eb emplace_back
+#define popf pop_front
+#define popb pop_back
+#define umap unordered_map
+#define uset unordered_set
+#define ell '\n'
+#define all(x) x.begin(), x.end()
+#define scan(x) do{while((x=getchar())<'0');for(x-='0';'0'<=(_=getchar());x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    int R, S;
+    cin >> R >> S;
+    cout << R * 8 + S * 3 - 28 << ell;
+}
