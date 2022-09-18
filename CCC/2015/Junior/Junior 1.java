37e63cd218eeb64c006fda4db88592ebebf3e5ce
ImRyzon <imtheryzon@gmail.com>
2022-09-18 11:24:31 -0400

    Create Junior 1.java

---
+++ CCC/2015/Junior/Junior 1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+        if (month > 2 || (month == 2 && day > 18)) {
+            System.out.println("After");
+        } else if (month == 2 && day == 18) {
+            System.out.println("Special");
+        } else if (month < 2 || (month == 2 && day < 18)) {
+            System.out.println("Before");
+        }
+    }
+}
